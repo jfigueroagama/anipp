6ecc0a0d473d2c232a223cac52e4a08b09fe8726
josue <jfigueroagama@redesvozdatos.com>
2014-04-14 19:35:29 -0700

    Added 'Como Pertenecer a ANIPP'

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ AnippApp::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
+  
   get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
 
   get    '/signup',  to: 'users#new'
   get    '/admin',   to: 'users#administration'
